95759fb0329c6645ca545229768bd733ebdd2bc3
Gift902 <byishimogift366@gmail.com>
2025-11-03 22:32:02 +0200

    prepared for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -7,22 +7,35 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from "cors";
 import cloudinary from './config/cloudinaryConfig.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 connectDB();
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
 
 app.use('/api/posts', PostsRoutes);
 app.use('/api/songs', songsRoutes);
 app.use('/api/instrumentals', instrumentalRoutes);
 app.use("/api/admin", adminAuthRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT:", PORT);
