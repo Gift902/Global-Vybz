e6ee48efb7c0e56644c522753f58eaa21e6ffb71
Gift902 <byishimogift366@gmail.com>
2025-11-05 08:21:55 +0200

    initial commit

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://https://global-vybz-z9t6.onrender.com/api"; // <-- change this to your real backend domain
+    : "https://https://global-vybz-2-nufs.onrender.com//api"; // <-- change this to your real backend domain
 
 const api = axios.create({
   baseURL: BASE_URL,
