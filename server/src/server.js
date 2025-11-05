429b42a94fc94091cf64b4fd6394ff8466aeaf07
Gift902 <byishimogift366@gmail.com>
2025-11-05 09:12:26 +0200

    initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -38,5 +38,5 @@ if(process.env.NODE_ENV === "production"){
 }
 
 app.listen(PORT, () => {
-    console.log("Server is running on PORT:", PORT);
+    console.log(`Server is running on PORT: ${PORT}`);
 });
